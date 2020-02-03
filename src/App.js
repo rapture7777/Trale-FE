11cd61f5124f6d65c8e3ce4f096572dbe77af187
Adam's MacBook Pro <macbook@Macbooks-MacBook-Pro.local>
2020-02-03 11:44:29 +0000

    implement routing and test pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './css/App.css';
+import { IonApp, IonContent, IonRouterOutlet } from '@ionic/react';
+import NavBar from './components/NavBar';
+import { Route, Redirect } from 'react-router-dom';
+import { IonReactRouter } from '@ionic/react-router';
 
 function App() {
-  return <div></div>;
+  return (
+    <IonApp>
+      <NavBar />
+    </IonApp>
+  );
 }
 
 export default App;
---
+++ src/components/Trails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IonTitle, IonPage } from '@ionic/react';
+
+const Trails = () => {
+  return (
+    <IonPage>
+      <IonTitle>Trails</IonTitle>
+    </IonPage>
+  );
+};
+
+export default Trails;
