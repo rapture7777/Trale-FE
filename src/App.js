84c7ab70cde5dd9d3cca83348261d01705049d7c
Adam's MacBook Pro <macbook@Macbooks-MacBook-Pro.local>
2020-02-14 08:46:45 +0000

    works on styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 import axios from 'axios';
+// import '@ionic/react/css/core.css';
+// import '@ionic/react/css/normalize.css';
+// import '@ionic/react/css/structure.css';
+// import '@ionic/react/css/typography.css';
+// import '@ionic/react/css/padding.css';
+// import '@ionic/react/css/float-elements.css';
+// import '@ionic/react/css/text-alignment.css';
+// import '@ionic/react/css/text-transformation.css';
+// import '@ionic/react/css/flex-utils.css';
+// import '@ionic/react/css/display.css';
 
 Amplify.configure(awsmobile);
 
